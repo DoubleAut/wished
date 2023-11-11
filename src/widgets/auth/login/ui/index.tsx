d2c4d9b87ece32fdf329eef9b80f8e0b53c2457b
RichardPickman <drezzerock@gmail.com>
2023-11-11 20:33:20 +0200

    add login widget that wraps features and entities together

---
+++ src/widgets/auth/login/ui/index.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { AuthCard } from '@/entities/auth';
+import {
+    LoginForm,
+    LoginHeader,
+    LoginProviders,
+    LoginQuestion,
+} from '@/features/auth/login';
+import { Inputs } from '@/shared/types/Auth';
+import { signIn } from 'next-auth/react';
+import { useSearchParams } from 'next/navigation';
+
+export const Login = () => {
+    const searchParams = useSearchParams();
+    const callbackUrl = searchParams.get('callbackUrl') ?? '/';
+
+    const onSubmit = async (data: Inputs) => {
+        const result = await signIn('credentials', { ...data, callbackUrl });
+
+        console.log(result);
+    };
+
+    return (
+        <AuthCard
+            header={<LoginHeader />}
+            form={<LoginForm onSubmit={onSubmit} />}
+            providers={<LoginProviders />}
+            question={
+                <LoginQuestion
+                    text="Don’t have an account?"
+                    type="Sign up"
+                    url="/auth/register"
+                />
+            }
+        />
+    );
+};
