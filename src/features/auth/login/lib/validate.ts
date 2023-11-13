816368ff93d013c1bb333997c22f422df5341ffb
RichardPickman <drezzerock@gmail.com>
2023-11-13 12:00:00 +0200

    move repetative validations to shared lib

--- src/features/auth/login/lib/validate.ts
+++ src/features/auth/login/lib/validate.ts
@@ -1,48 +1,11 @@
-import { Errors, Inputs } from '@/shared/types/Auth';
-
-const isBlank = (input: string) => input.length === 0;
-
-const isValidEmail = (input: string) => {
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    const isValid = emailRegex.test(input);
-
-    return isValid;
-};
+import { validateEmail, validatePassword } from '@/shared/lib/authValidation';
+import { Inputs } from '@/shared/types/Auth';
 
 export const validate = (inputs: Inputs) => {
     const { email, password } = inputs;
-    const errors: Errors = {
-        email: {
-            isError: false,
-            messages: [],
-        },
-        password: {
-            isError: false,
-            messages: [],
-        },
+
+    return {
+        email: validateEmail(email),
+        password: validatePassword(password),
     };
-
-    if (isBlank(email)) {
-        errors.email.isError = true;
-        errors.email.messages.push('Email cannot be blank');
-    }
-
-    if (email && !isValidEmail(email)) {
-        errors.email.isError = true;
-        errors.email.messages.push('Email address is wrong');
-    }
-
-    if (isBlank(password)) {
-        errors.password.isError = true;
-        errors.password.messages.push('Password cannot be blank');
-    }
-
-    if (password && password.length < 5) {
-        errors.password.isError = true;
-        errors.password.messages.push(
-            'Password cannot be less then 5 characters',
-        );
-    }
-
-    return errors;
 };
--- src/features/auth/register/lib/validate.ts
+++ src/features/auth/register/lib/validate.ts
@@ -1,57 +1,21 @@
+import { validateEmail, validatePassword } from '@/shared/lib/authValidation';
 import { Errors, Inputs } from '@/shared/types/Auth';
-
-const isBlank = (input: string) => input.length === 0;
-
-const isValidEmail = (input: string) => {
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    const isValid = emailRegex.test(input);
-
-    return isValid;
-};
 
 export const validate = (inputs: Required<Inputs>) => {
     const { email, password, passwordRepeat } = inputs;
-    const errors: Required<Errors> = {
-        email: {
-            isError: false,
-            messages: [],
-        },
-        password: {
-            isError: false,
-            messages: [],
-        },
-        passwordRepeat: {
-            isError: false,
-            messages: [],
-        },
+    const errors: Errors['passwordRepeat'] = {
+        isError: false,
+        messages: [],
     };
 
-    if (isBlank(email)) {
-        errors.email.isError = true;
-        errors.email.messages.push('Email cannot be blank');
+    if (passwordRepeat.length === 0 || passwordRepeat !== password) {
+        errors.isError = true;
+        errors.messages.push('Password is not the same');
     }
 
-    if (email && !isValidEmail(email)) {
-        errors.email.isError = true;
-        errors.email.messages.push('Email is not valid');
-    }
-
-    if (isBlank(password)) {
-        errors.password.isError = true;
-        errors.password.messages.push('Password cannot be blank');
-    }
-
-    if (password && password.length < 5) {
-        errors.password.isError = true;
-        errors.password.messages.push(
-            'Password cannot be less then 5 characters',
-        );
-    }
-
-    if (isBlank(passwordRepeat) || passwordRepeat !== password) {
-        errors.passwordRepeat.isError = true;
-        errors.passwordRepeat.messages.push('Password is not the same');
-    }
-
-    return errors;
+    return {
+        email: validateEmail(email),
+        password: validatePassword(password),
+        passwordRepeat: errors,
+    };
 };
