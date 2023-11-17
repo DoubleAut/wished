f27abe8c66d3f048b0b63499c70e8dc6f77ba1bf
RichardPickman <drezzerock@gmail.com>
2023-11-17 16:49:12 +0100

    update main page with utilizing current JWT session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,37 @@
 'use client';
 
 import { useSession, signIn, signOut } from 'next-auth/react';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useLayoutEffect } from 'react';
 
 export default function Home() {
     const { data: session } = useSession();
+    const navigation = useRouter();
 
-    if (session) {
-        return (
-            <>
-                Signed in as {session.user?.email} <br />
-                <button onClick={() => signOut()}>Sign out</button>
-            </>
-        );
-    }
+    useLayoutEffect(() => {
+        // If session return refresh token error, then user will need to relogin!
+        if (session?.error === 'RefreshAccessTokenError') {
+            navigation.push(`/auth/error?error=Session expired`);
+        }
+    }, [session]);
+
     return (
         <>
-            Not signed in <br />
-            <button onClick={() => signIn()}>Sign in</button>
+            {session && (
+                <>
+                    Signed in as {session.user?.email} <br />
+                    <button onClick={() => signOut()}>Sign out</button>
+                </>
+            )}
+            {!session && (
+                <>
+                    Not signed in <br />
+                    <button onClick={() => signIn()}>Sign in</button>
+                </>
+            )}
+
+            <Link href="/profile">Profile!</Link>
         </>
     );
 }
