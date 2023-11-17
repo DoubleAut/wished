dc11cc248693608d7485331480937a2814da812a
RichardPickman <drezzerock@gmail.com>
2023-11-17 16:46:38 +0100

    add util functions for user registration

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,8 +1,5 @@
 import { NextResponse } from 'next/server';
-
-type Credentials = Record<'email' | 'password', string> | undefined;
-type Data = Credentials & { passwordRepeat: string };
-type User = Credentials & { id: number };
+import { createUser, getUserByEmail } from '../[...nextauth]/services/user';
 
 const errorSkeleton = {
     email: {
@@ -19,51 +16,10 @@ const errorSkeleton = {
     },
 };
 
-const fetchUsers = async (url: string) => {
-    const response = await fetch(url, { method: 'GET' });
-
-    if (!response.ok) {
-        throw new Error('Error during getting users');
-    }
-
-    const result: User[] = await response.json();
-
-    return result;
-};
-
-const getUser = async (email: string) => {
-    const users: User[] = await fetchUsers('http://localhost:5000/users');
-    const user = users.find(user => user.email === email);
-
-    return user;
-};
-
-const createUser = async (credentials: Data) => {
-    const { email, password } = credentials;
-    const options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, password }),
-    };
-
-    const response = await fetch('http://localhost:5000/users', options);
-    const user: User = await response.json();
-
-    return user;
-};
-
-const isUserExists = async (email: string) => {
-    const user = await getUser(email);
-
-    return !!user;
-};
-
 const handler = async (req: Request) => {
     try {
-        const request: Data = await req.json();
-        const isUserExist = await isUserExists(request.email);
+        const request = await req.json();
+        const isUserExist = await getUserByEmail(request.email);
 
         if (isUserExist) {
             const body = JSON.stringify({
@@ -77,7 +33,10 @@ const handler = async (req: Request) => {
             return new NextResponse(body, { status: 400 });
         }
 
-        const user = await createUser(request);
+        const user = await createUser({
+            email: request.email,
+            password: request.password,
+        });
 
         return new NextResponse(JSON.stringify(user), { status: 201 });
     } catch (e) {
