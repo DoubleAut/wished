ce9a6036b34b4e38f8d9e49190279dfd257695ef
RichardPickman <drezzerock@gmail.com>
2024-05-09 11:36:45 +0200

    update project configs and update packages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'www.startpage.com',
+                port: '',
+                pathname: '/av/**',
+            },
+        ],
+    },
+}
 
 module.exports = nextConfig
