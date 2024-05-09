c8df6c6e5a146b7241fdd65eeff832a1dcf3455a
RichardPickman <drezzerock@gmail.com>
2024-05-09 11:41:15 +0200

    add user interceptor

--- src/shared/lib/interceptors/User.tsx
+++ src/shared/lib/interceptors/User.tsx
@@ -1,28 +1,21 @@
 'use client';
 
-import { RootState } from '@/core/store';
-import { getUserByEmail } from '@/entities/user/lib';
-import { setUser } from '@/entities/user/model';
+import { setUser, userStore } from '@/entities/user/model/userStore';
 import { useSession } from 'next-auth/react';
-import { ReactNode, useLayoutEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { ReactNode } from 'react';
+import { useStore } from 'zustand';
 
 export const UserInterceptor = ({ children }: { children: ReactNode }) => {
-    const user = useSelector((state: RootState) => state.user);
+    const user = useStore(userStore, state => state.user);
     const { data: session } = useSession();
-    const dispatch = useDispatch();
 
-    useLayoutEffect(() => {
-        if (session?.user && !user) {
-            const handleUser = async () => {
-                const response = await getUserByEmail(session.user.email);
+    const handleUser = async () => {
+        await setUser(session!.user.email);
+    };
 
-                dispatch(setUser(response));
-            };
-
-            handleUser();
-        }
-    }, [session, user]);
+    if (session?.user && !user) {
+        handleUser();
+    }
 
     return children;
 };
