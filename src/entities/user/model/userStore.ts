e5301f64a181b49de12c06c5ea8cbd26984d8bca
RichardPickman <drezzerock@gmail.com>
2024-05-12 11:42:37 +0200

    update user state

--- src/entities/user/model/userStore.ts
+++ src/entities/user/model/userStore.ts
@@ -1,16 +1,41 @@
-import { User } from '@/shared/types/User';
+import { User, Wish } from '@/shared/types/User';
+import { Nullable } from '@/shared/types/Utility/Nullable';
 import { createStore } from 'zustand';
 
-export interface UserStore {
-    user: User | null;
+type UserState = Nullable<User>;
+
+export interface UserActions {
+    setUser: (user: User) => void;
+    setFollowers: (followers: User[]) => void;
+    setFollowings: (followings: User[]) => void;
+    setWishes: (wishes: Wish[]) => void;
 }
-// TODO: Add store handler
-export const createUserStore = createStore<UserStore>()(() => ({
-    user: null,
-}));
 
-export const getUser = (req: Request) => {
-    const store = createUserStore.getState();
+export type UserStore = UserState & UserActions;
 
-    return store.user;
+export const defaultInitState = {
+    id: null,
+    email: '',
+    name: '',
+    surname: '',
+    picture: '',
+    isActive: false,
+    followings: [],
+    followers: [],
+    wishes: [],
+    reservations: [],
 };
+
+export const createUserStore = (initState: UserState = defaultInitState) => {
+    return createStore<UserStore>()(set => ({
+        ...initState,
+        setUser: (user: User) => set(() => ({ ...user })),
+        setFollowers: followers => set(() => ({ followers })),
+        setFollowings: followings => set(() => ({ followings })),
+        setWishes: wishes => set(() => ({ wishes })),
+    }));
+};
+// TODO: Add store handler
+// export const createUserStore = createStore<UserStore>()(() => ({
+//     user: null,
+// }));
