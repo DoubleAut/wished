23908848eebf68791719b3215ff44e1b163ff448
RichardPickman <drezzerock@gmail.com>
2024-05-12 11:43:26 +0200

    update project providers due to zustand state integration

--- src/core/providers/index.tsx
+++ src/core/providers/index.tsx
@@ -1,14 +1,12 @@
 'use client';
 
-import { Session } from 'next-auth';
 import { ReactNode } from 'react';
-import SessionProvider from './Auth';
+import { UserStoreProvider } from './UserProvider';
 
 interface Props {
     children: ReactNode;
-    session: Session | null;
 }
 
-export const Providers = ({ children, session }: Props) => {
-    return <SessionProvider session={session}>{children}</SessionProvider>;
+export const Providers = ({ children }: Props) => {
+    return <UserStoreProvider>{children}</UserStoreProvider>;
 };
