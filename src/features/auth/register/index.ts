707518521b783700c5247405fc22f02d195bca33
RichardPickman <drezzerock@gmail.com>
2024-05-12 19:22:45 +0200

    update register once again

--- src/features/auth/register/index.ts
+++ src/features/auth/register/index.ts
@@ -1,11 +1,3 @@
 import { RegistrationForm } from './ui/Form';
-import { Header as RegisterHeader } from './ui/Header';
-import { Providers as RegisterProviders } from './ui/Providers';
-import { Question as RegisterQuestion } from './ui/Question';
 
-export {
-    RegisterHeader,
-    RegisterProviders,
-    RegisterQuestion,
-    RegistrationForm,
-};
+export { RegistrationForm };
--- src/features/auth/register/ui/Header.tsx
+++
@@ -1,5 +0,0 @@
-import { Heading } from '@/shared/ui/Heading';
-
-export const Header = () => (
-    <Heading header="Register" subheader="Please enter you details" />
-);
