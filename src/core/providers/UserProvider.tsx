8181d08daaea5107da4f4591fb9f00152a98e350
RichardPickman <drezzerock@gmail.com>
2024-05-12 11:43:43 +0200

    add store provider for user entity

---
+++ src/core/providers/UserProvider.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { UserStore, createUserStore } from '@/entities/user/model/userStore';
+import { createContext, useContext, useRef, type ReactNode } from 'react';
+import { useStore, type StoreApi } from 'zustand';
+
+export const UserStoreContext = createContext<StoreApi<UserStore> | null>(null);
+
+export interface UserStoreProviderProps {
+    children: ReactNode;
+}
+
+export const UserStoreProvider = ({ children }: UserStoreProviderProps) => {
+    const storeRef = useRef<StoreApi<UserStore>>();
+
+    if (!storeRef.current) {
+        storeRef.current = createUserStore();
+    }
+
+    return (
+        <UserStoreContext.Provider value={storeRef.current}>
+            {children}
+        </UserStoreContext.Provider>
+    );
+};
+
+export const useUserStore = <T,>(selector: (store: UserStore) => T): T => {
+    const userStoreContext = useContext(UserStoreContext);
+
+    if (!userStoreContext) {
+        throw new Error(`useUserStore must be use within UserStoreProvider`);
+    }
+
+    return useStore(userStoreContext, selector);
+};
