5a83d7fe7b7651cec2548786e9979b07cebbf016
RichardPickman <drezzerock@gmail.com>
2024-05-17 16:37:39 +0200

    add header widget on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 'use client';
 
 import { useUserStore } from '@/core/providers/UserProvider';
+import { Navigation } from '@/shared/ui/Navigation';
+import { Avatar, AvatarImage } from '@/shared/ui/avatar';
 import { Button } from '@/shared/ui/button';
+import { HeaderWidget } from '@/widgets/header';
 import Link from 'next/link';
 
 export default function Home() {
@@ -9,22 +12,33 @@ export default function Home() {
 
     return (
         <>
-            {user && (
-                <>
-                    Signed in as {user.email} <br />
-                    <button>Sign out</button>
-                </>
-            )}
-            {!user && (
-                <>
-                    Not signed in <br />
-                    <Button variant="link">
-                        <Link href="/auth/login">Sign in</Link>
-                    </Button>
-                </>
-            )}
+            <HeaderWidget
+                logo={'WISHER'}
+                links={<Navigation />}
+                profile={
+                    <Avatar className="h-10 w-10">
+                        <AvatarImage src="avatar_not_found.png" />
+                    </Avatar>
+                }
+            />
+            <div className="container flex flex-col items-center">
+                {user && (
+                    <>
+                        Signed in as {user.email} <br />
+                        <button>Sign out</button>
+                    </>
+                )}
+                {!user && (
+                    <>
+                        Not signed in <br />
+                        <Button variant="link">
+                            <Link href="/auth/login">Sign in</Link>
+                        </Button>
+                    </>
+                )}
 
-            <Link href="/profile">Profile!</Link>
+                <Link href="/profile">Profile!</Link>
+            </div>
         </>
     );
 }
