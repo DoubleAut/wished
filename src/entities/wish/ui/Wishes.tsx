e5b6e73b24028bd5654377041b235b7ae611c13b
RichardPickman <drezzerock@gmail.com>
2024-05-21 16:00:12 +0200

    update wish ui components

--- src/entities/wish/ui/Wishes.tsx
+++ src/entities/wish/ui/Wishes.tsx
@@ -1,70 +1,66 @@
 import { Wish as IWish } from '@/shared/types/Wish';
 import { Subheader } from '@/shared/ui/Text/subheader';
 import { Button } from '@/shared/ui/button';
-import { MoreHorizontal } from 'lucide-react';
+import { Skeleton } from '@/shared/ui/skeleton';
 import Image from 'next/image';
-import Link from 'next/link';
 
 interface WishesProps {
-    subheader: string;
-    wishes: IWish[];
+    wishes?: IWish[];
 }
 
-export const Wishes = ({ subheader, wishes }: WishesProps) => (
-    <div className="flex flex-col gap-3">
-        <Subheader>{subheader}</Subheader>
-        <div className="grid grid-cols-4 gap-3">
+export const Wishes = ({ wishes }: WishesProps) => {
+    if (!wishes) {
+        return (
+            <div className="flex flex-col gap-3">
+                <Subheader>
+                    <Skeleton />
+                </Subheader>
+                <div className="grid grid-cols-4 gap-3">
+                    <Skeleton />
+                    <Skeleton />
+                    <Skeleton />
+                    <Skeleton />
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
             {wishes.map(item => (
                 <Wish key={item.id} wish={item} />
             ))}
         </div>
-    </div>
-);
+    );
+};
 
-export const MakeWish = ({ href }: { href: string }) => (
-    <Link href={href}>
-        <Button className="w-full">+ Make a wish</Button>
-    </Link>
-);
+export const Wish = ({ wish }: { wish: IWish }) => {
+    const date = new Date(wish.created_at);
+    const splitted = [date.getDay(), date.getMonth(), date.getFullYear()];
 
-export const Wish = ({ wish }: { wish: IWish }) => (
-    <div className="aspect-square rounded-md bg-slate-500 p-1">
-        <div className="relative">
-            {wish.picture && (
-                <>
-                    <div className="relative aspect-square w-full">
-                        <Image
-                            className="object-contain"
-                            src={wish.picture}
-                            alt="Wish picture"
-                            fill
-                        />
-                    </div>
-                    <div className="absolute right-0 top-0">
-                        <MoreHorizontal />
-                    </div>
-                </>
-            )}
-
-            {!wish.picture && (
-                <>
-                    <div className="relative aspect-square w-full">
-                        <Image
-                            className="object-contain"
-                            src="/avatar_not_found.png"
-                            alt="Wish picture"
-                            fill
-                        />
-                    </div>
-                    <div className="absolute right-0 top-0">
-                        <MoreHorizontal />
-                    </div>
-                </>
-            )}
+    return (
+        <div className="flex w-full flex-col">
+            <div className="relative aspect-square overflow-hidden rounded">
+                <Image
+                    className="object-cover"
+                    src={wish.picture ?? '/avatar_not_found.png'}
+                    alt="Wish picture"
+                    sizes="(max-width: 640px) 320px, (max-width: 768px) 200px, (max-width: 1024px) 200px"
+                    fill
+                />
+                <Button
+                    className="absolute right-0 top-0 rounded-full"
+                    variant="ghost"
+                    size="icon"
+                ></Button>
+            </div>
+            <div className="flex flex-col space-y-2 px-3 py-3">
+                <div>{wish.title}</div>
+                <div className="flex justify-between">
+                    <p>{wish.price}</p>
+                    <p>{splitted.join('.')}</p>
+                </div>
+            </div>
         </div>
-        <div className="flex flex-col gap-2 text-white">
-            <p className="text-md font-bold">{wish.title}</p>
-            {wish.price && <p className="text-sm">{wish.price}</p>}
-        </div>
-    </div>
-);
+    );
+};
