f5d23257cdc513688d272a8fd0b7dd87c2c3345a
RichardPickman <drezzerock@gmail.com>
2024-05-21 15:59:52 +0200

    update main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,21 @@
 'use client';
 
-import { useUserStore } from '@/core/providers/UserProvider';
-import { Navigation } from '@/shared/ui/Navigation';
-import { Avatar, AvatarImage } from '@/shared/ui/avatar';
-import { Button } from '@/shared/ui/button';
-import { HeaderWidget } from '@/widgets/header';
-import Link from 'next/link';
+import { useViewerStore } from '@/core/providers/ViewerProvider';
+import { Wishes } from '@/entities/wish/ui/Wishes';
+import { WishDialog } from '@/features/wish/ui/WishDialog';
 
-export default function Home() {
-    const user = useUserStore(state => state.user);
+const Home = () => {
+    const { user } = useViewerStore(state => state);
 
     return (
-        <>
-            <HeaderWidget
-                logo={'WISHER'}
-                links={<Navigation />}
-                profile={
-                    <Avatar className="h-10 w-10">
-                        <AvatarImage src="avatar_not_found.png" />
-                    </Avatar>
-                }
-            />
-            <div className="container flex flex-col items-center">
-                {user && (
-                    <>
-                        Signed in as {user.email} <br />
-                        <button>Sign out</button>
-                    </>
-                )}
-                {!user && (
-                    <>
-                        Not signed in <br />
-                        <Button variant="link">
-                            <Link href="/auth/login">Sign in</Link>
-                        </Button>
-                    </>
-                )}
-
-                <Link href="/profile">Profile!</Link>
+        <div className="container flex flex-col space-y-10">
+            <div className="flex items-center space-x-4 uppercase">
+                <p>My wishes</p>
+                <WishDialog />
             </div>
-        </>
+            <Wishes wishes={user?.wishes} />
+        </div>
     );
-}
+};
+
+export default Home;
