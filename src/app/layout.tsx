f8e80e9940ea23ad7bc8406eef661545088d3d7c
RichardPickman <drezzerock@gmail.com>
2024-05-21 15:59:41 +0200

    add global header and update providers in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,12 @@ import '@/core/styles/globals.css';
 import '@uploadthing/react/styles.css';
 
 import { Providers } from '@/core/providers';
+import { NAVIGATION_LINKS } from '@/shared/lib/constants/Links';
+import { AuthHeader } from '@/shared/ui/AuthHeader';
+import { Navigation } from '@/shared/ui/Navigation';
+import { ModeToggle } from '@/shared/ui/Theme';
 import { Toaster } from '@/shared/ui/sonner';
+import { HeaderWidget } from '@/widgets/header';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ReactNode } from 'react';
@@ -10,18 +15,30 @@ import { ReactNode } from 'react';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-    title: 'Wished',
+    title: 'WISHED',
     description: 'Add and reserve wishes!',
 };
 
 async function RootLayout({ children }: { children: ReactNode }) {
     return (
-        <html lang="en">
+        <html lang="en" suppressHydrationWarning>
             <body className={inter.className}>
-                <main className="relative flex w-full flex-col items-center justify-center">
-                    <Providers>{children}</Providers>
-                </main>
-                <Toaster />
+                <Providers>
+                    <main className="relative flex w-full flex-col items-center justify-center space-y-2">
+                        <HeaderWidget
+                            logo={'WISHED'}
+                            links={<Navigation links={NAVIGATION_LINKS} />}
+                            profile={
+                                <div className="flex space-x-1">
+                                    <ModeToggle />
+                                    <AuthHeader />
+                                </div>
+                            }
+                        />
+                        {children}
+                    </main>
+                    <Toaster />
+                </Providers>
             </body>
         </html>
     );
