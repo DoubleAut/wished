fcabba7889e9456ef664e22901db7cc9e65cf21f
RichardPickman <drezzerock@gmail.com>
2024-05-23 13:10:48 +0200

    add dropdown menu to user avatar in header

--- src/shared/ui/AuthHeader/index.tsx
+++ src/shared/ui/AuthHeader/index.tsx
@@ -2,8 +2,16 @@
 
 import { useViewerStore } from '@/core/providers/ViewerProvider';
 import { AUTH_LINKS } from '@/shared/lib/constants/Links';
+import { RiProfileLine, RiSettings2Line } from '@remixicon/react';
+import Link from 'next/link';
 import { Navigation } from '../Navigation';
 import { Avatar, AvatarImage } from '../avatar';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from '../dropdown-menu';
 
 export const AuthHeader = () => {
     const user = useViewerStore(state => state.user);
@@ -13,8 +21,26 @@ export const AuthHeader = () => {
     }
 
     return (
-        <Avatar className="h-10 w-10">
-            <AvatarImage src="avatar_not_found.png" />
-        </Avatar>
+        <DropdownMenu>
+            <DropdownMenuTrigger className="cursor-pointer" asChild>
+                <Avatar className="h-10 w-10">
+                    <AvatarImage src={user.picture ?? 'avatar_not_found.png'} />
+                </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuItem className="cursor-pointer space-x-2" asChild>
+                    <Link href="/profile">
+                        <RiProfileLine />
+                        <p>Profile</p>
+                    </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem className="cursor-pointer space-x-2" asChild>
+                    <Link href="/settings">
+                        <RiSettings2Line />
+                        <p>Settings</p>
+                    </Link>
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
     );
 };
