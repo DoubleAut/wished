daf7ae0d1675705477edaf3b894e025e9d4c4e92
RichardPickman <drezzerock@gmail.com>
2024-05-24 13:52:57 +0200

    add wishes and reservations merger to display all wishes at once

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,32 @@
 'use client';
 
 import { useViewerStore } from '@/core/providers/ViewerProvider';
+import { WishDialog } from '@/entities/wish/ui/WishDialog';
+import { WishForm } from '@/entities/wish/ui/WishForm';
 import { Wishes } from '@/entities/wish/ui/Wishes';
-import { WishDialog } from '@/features/wish/ui/WishDialog';
+import { Button } from '@/shared/ui/button';
+import { useMemo } from 'react';
 
 const Home = () => {
     const { user } = useViewerStore(state => state);
+
+    const allWishes = useMemo(() => {
+        const wishes = user?.wishes ?? [];
+        const reservations = user?.reservations ?? [];
+
+        return [...wishes, ...reservations];
+    }, [user?.wishes, user?.reservations]);
 
     return (
         <div className="container flex flex-col space-y-10">
             <div className="flex items-center space-x-4 uppercase">
                 <p>My wishes</p>
-                <WishDialog />
+                <WishDialog
+                    trigger={<Button variant="outline">Make a wish</Button>}
+                    content={<WishForm onCancel={() => {}} />}
+                />
             </div>
-            <Wishes wishes={user?.wishes} />
+            <Wishes wishes={allWishes} />
         </div>
     );
 };
