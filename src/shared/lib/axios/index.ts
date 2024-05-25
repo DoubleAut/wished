ce03ac300f59bbb66589d0f8851c682d35bb5830
RichardPickman <drezzerock@gmail.com>
2024-05-25 14:43:09 +0200

    remove axios and add middleware for fetch

--- src/shared/lib/axios/index.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const apiURL = process.env.NEXT_PUBLIC_BACKEND_API;
-
-if (!apiURL) {
-    throw new Error('There is no backend api address!!!');
-}
-
-export const fetcher = axios.create({ baseURL: apiURL });
