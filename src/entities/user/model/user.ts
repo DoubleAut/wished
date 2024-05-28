f724bfc8cc3abe8434674db8ff69f134e976896d
RichardPickman <drezzerock@gmail.com>
2024-05-28 13:16:16 +0200

    divide old user/friend stores into own slices for global store

--- src/entities/user/model/user.ts
+++ src/entities/user/model/user.ts
@@ -1,27 +1,7 @@
-import { PlainUser, UserWithFriends } from '@/shared/types/User';
-import { StateCreator, create } from 'zustand';
-import { getUser } from '../lib/user';
+import { WishesSlice } from '@/entities/wish/model/wishesStore';
+import { PlainUser } from '@/shared/types/User';
+import { StateCreator } from 'zustand';
 import { FriendsSlice } from './friendsStore';
-import { WishesSlice } from './wishesStore';
-
-interface UserStore {
-    user: UserWithFriends | null;
-    setUser: (user: UserWithFriends) => void;
-}
-
-export const userStore = create<UserStore>()(set => ({
-    user: null,
-    setUser: (user: UserWithFriends) => {
-        set({ user });
-    },
-}));
-
-export const setInitialUser = async (id: number) => {
-    const user = await getUser(id);
-
-    userStore.setState({ user });
-};
-
 export interface UserInformationSlice {
     user: PlainUser | null;
     setUser: (user: PlainUser) => void;
