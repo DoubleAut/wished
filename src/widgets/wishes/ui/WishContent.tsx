5092bc0463961c7a69851498be5d7ee737b8dfbc
RichardPickman <drezzerock@gmail.com>
2024-05-28 13:06:36 +0200

    update wishes content, that opens inside of a dialog

--- src/widgets/wishes/ui/WishContent.tsx
+++ src/widgets/wishes/ui/WishContent.tsx
@@ -1,7 +1,10 @@
+'use client';
+
 import { useViewerStore } from '@/core/providers/ViewerProvider';
-import { viewWishStore } from '@/entities/wish/model/wishStore';
 import { Background } from '@/entities/wish/ui/WishCard';
+import { dialogStore } from '@/features/wish/model/dialogView';
 import { Wish } from '@/shared/types/Wish';
+import { Typography } from '@/shared/ui/Text/typography';
 import { Badge } from '@/shared/ui/badge';
 import { Skeleton } from '@/shared/ui/skeleton';
 import Image from 'next/image';
@@ -14,48 +17,49 @@ interface Props {
 }
 
 /**
- * WishContent is a component, that is being rendered inside of WishDialog. Current content displayed only if ViewWishStore has 'view' state.
+ * WishContent is a component, that is being rendered inside of WishDialog. Current content displayed only if dialogStore has 'view' state.
  *
  * @param actions - Box with buttons. May vary depends on which Wish is it. If Wish is your own, displays own wish logic buttons, otherwise wish is not users, which has different logic buttons.
  *
  * @returns dialog component with wish and its controls
  */
 
+export const WishContentSkeleton = () => (
+    <div className="flex h-full flex-col items-start gap-6 rounded-lg p-6 md:flex-row">
+        <div className="relative h-full w-full flex-shrink-0 overflow-hidden rounded-lg md:w-1/2">
+            <Skeleton className="inset-0" />
+        </div>
+        <div className="flex h-full flex-1 flex-col justify-between space-y-4">
+            <h3 className="text-2xl font-bold">
+                <Skeleton />
+            </h3>
+            <div className="flex gap-2">
+                <Badge variant="outline">
+                    <Skeleton />
+                </Badge>
+                <Badge variant="outline">
+                    <Skeleton />
+                </Badge>
+            </div>
+            <p className="text-lg leading-relaxed text-gray-500 dark:text-gray-400">
+                <Skeleton />
+            </p>
+            <div className="text-3xl font-bold">
+                <Skeleton />
+            </div>
+            <Skeleton />
+        </div>
+    </div>
+);
+
 export const WishContent = ({ actions }: Props) => {
-    const viewerStore = useViewerStore(state => state);
-    const { wish } = useStore(viewWishStore);
-    const isBadgesVisible = viewerStore.user?.id === wish?.owner.id;
+    const store = useStore(dialogStore);
+    const viewerId = useViewerStore(state => state.user?.id);
+    const wish = store.dialogWish as Wish;
+    const isBadgesVisible = viewerId === wish.owner.id;
 
     if (!wish) {
-        return (
-            <div className="flex h-full flex-col items-start gap-6 rounded-lg p-6 md:flex-row">
-                <div className="relative h-full w-full flex-shrink-0 overflow-hidden rounded-lg md:w-1/2">
-                    <Skeleton className="inset-0" />
-                </div>
-                <div className="flex h-full flex-1 flex-col justify-between space-y-4">
-                    <h3 className="text-2xl font-bold">
-                        <Skeleton />
-                    </h3>
-                    {isBadgesVisible && (
-                        <div className="flex gap-2">
-                            <Badge variant="outline">
-                                <Skeleton />
-                            </Badge>
-                            <Badge variant="outline">
-                                <Skeleton />
-                            </Badge>
-                        </div>
-                    )}
-                    <p className="text-lg leading-relaxed text-gray-500 dark:text-gray-400">
-                        <Skeleton />
-                    </p>
-                    <div className="text-3xl font-bold">
-                        <Skeleton />
-                    </div>
-                    {actions}
-                </div>
-            </div>
-        );
+        return <WishContentSkeleton />;
     }
 
     return (
@@ -72,8 +76,8 @@ export const WishContent = ({ actions }: Props) => {
                     <Background isHover={false} text={wish.title} />
                 )}
             </div>
-            <div className="flex h-full flex-1 flex-col justify-between space-y-4">
-                <h3 className="text-2xl font-bold">{wish.title}</h3>
+            <div className="flex flex-col justify-between gap-4">
+                <Typography variant="h3">{wish.title}</Typography>
                 {isBadgesVisible && (
                     <div className="flex gap-2">
                         <Badge variant="outline">
@@ -84,10 +88,10 @@ export const WishContent = ({ actions }: Props) => {
                         </Badge>
                     </div>
                 )}
-                <p className="text-lg leading-relaxed text-gray-500 dark:text-gray-400">
-                    {wish.description}
-                </p>
-                <div className="text-3xl font-bold">${wish.price}</div>
+                <Typography variant="paragraph">{wish.description}</Typography>
+                <Typography variant="h3" className="self-end">
+                    ${wish.price}
+                </Typography>
                 {actions}
             </div>
         </div>
