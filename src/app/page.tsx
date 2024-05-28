c6e14d556fe09534cafdadc74eb4c5c7fbeb38d0
RichardPickman <drezzerock@gmail.com>
2024-05-28 13:19:49 +0200

    update main page due to new wishes system integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,26 +4,29 @@ import { useViewerStore } from '@/core/providers/ViewerProvider';
 import { WishDialog } from '@/entities/wish/ui/WishDialog';
 import { WishForm } from '@/entities/wish/ui/WishForm';
 import { Wishes } from '@/entities/wish/ui/Wishes';
-import { Button } from '@/shared/ui/button';
 import { useMemo } from 'react';
 
 const Home = () => {
-    const { user } = useViewerStore(state => state);
+    const user = useViewerStore(state => state.user);
+    const wishes = useViewerStore(state => state.wishes);
+    const reservations = useViewerStore(state => state.reservations);
 
     const allWishes = useMemo(() => {
-        const wishes = user?.wishes ?? [];
-        const reservations = user?.reservations ?? [];
-
         return [...wishes, ...reservations];
-    }, [user?.wishes, user?.reservations]);
+    }, [wishes, reservations]);
+
+    if (!user) {
+        return <div>Loading</div>;
+    }
 
     return (
-        <div className="container flex flex-col space-y-10">
+        <div className="container flex flex-col">
             <div className="flex items-center space-x-4 uppercase">
                 <p>My wishes</p>
                 <WishDialog
-                    trigger={<Button variant="outline">Make a wish</Button>}
+                    trigger={'Make a wish'}
                     content={<WishForm onCancel={() => {}} />}
+                    wish={null}
                 />
             </div>
             <Wishes wishes={allWishes} />
