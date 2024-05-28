770be8178ad550e7c1ab25e837eb8db6521cf155
RichardPickman <drezzerock@gmail.com>
2024-05-28 13:26:48 +0200

    update header styles

--- src/widgets/header/index.tsx
+++ src/widgets/header/index.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export const HeaderWidget = ({ logo, links, profile }: Props) => {
     return (
-        <header className="sticky top-0 z-50 w-full border-b">
+        <header className="sticky top-0 z-50 w-full border-b bg-background backdrop-blur-xl">
             <div className="container flex h-14 w-full max-w-screen-2xl items-center justify-between">
                 {logo}
                 {links}
