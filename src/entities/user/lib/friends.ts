64f47867aa3378ad9f447d481be999233f187d9c
RichardPickman <drezzerock@gmail.com>
2024-05-28 13:16:32 +0200

    update friend actions

---
+++ src/entities/user/lib/friends.ts
@@ -0,0 +1,8 @@
+import { get } from '@/shared/api/Fetch';
+import { UserWithFriends } from '@/shared/types/User';
+
+export const getUserWithFriends = async (friendId: number) => {
+    const response = await get<UserWithFriends>(`/users/${friendId}/friends`);
+
+    return response;
+};
