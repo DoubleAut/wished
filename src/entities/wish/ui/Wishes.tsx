265dd6bc4c6ef43ccaa6c2761675e126245cd2bb
RichardPickman <drezzerock@gmail.com>
2024-05-28 13:04:45 +0200

    update wishes list

--- src/entities/wish/ui/Wishes.tsx
+++ src/entities/wish/ui/Wishes.tsx
@@ -1,29 +1,20 @@
-import { useViewerStore } from '@/core/providers/ViewerProvider';
-import { Wish as IWish, Wish } from '@/shared/types/Wish';
 import { Subheader } from '@/shared/ui/Text/subheader';
 
+import { dialogStore } from '@/features/wish/model/dialogView';
 import { DeleteWish } from '@/features/wish/ui/DeleteWish';
 import { EditWish } from '@/features/wish/ui/EditWish';
 import { HideWish } from '@/features/wish/ui/HideWish';
 import { ReserveWish } from '@/features/wish/ui/ReserveWish';
-import { User } from '@/shared/types/User';
+import { Wish as IWish } from '@/shared/types/Wish';
 import { Button } from '@/shared/ui/button';
 import { Skeleton } from '@/shared/ui/skeleton';
-import { WishContent } from '@/widgets/wishes/ui/WishContent';
 import { useStore } from 'zustand';
-import { viewWishStore } from '../model/wishStore';
-import { WishCard } from './WishCard';
-import { WishDialog } from './WishDialog';
-import { WishForm } from './WishForm';
-
-interface WishesProps {
-    wishes?: IWish[];
-}
+import { Wish } from './Wish';
 
 export const PersonalWishActions = () => {
-    const wishStore = useStore(viewWishStore);
+    const dialogWish = useStore(dialogStore);
 
-    if (!wishStore.wish) {
+    if (!dialogWish) {
         return (
             <div className="flex flex-col gap-2 sm:flex-row">
                 <Button className="w-full" asChild>
@@ -56,89 +47,26 @@ export const UserWishActions = () => {
     );
 };
 
-const getVisibleWishOrNull = (wish: Wish, viewer: User) => {
-    const isOwnWish = wish.owner.id === viewer?.id;
+export const WishesSkeleton = () => (
+    <div className="flex flex-col gap-3">
+        <Subheader>
+            <Skeleton className="h-full w-full" />
+        </Subheader>
+        <div className="grid grid-cols-4 gap-3">
+            <Skeleton className="h-full w-full" />
+            <Skeleton className="h-full w-full" />
+            <Skeleton className="h-full w-full" />
+            <Skeleton className="h-full w-full" />
+        </div>
+    </div>
+);
 
-    // Wish is hidden and current user is not wish owner
-    if (wish.isHidden && !isOwnWish) {
-        return null;
-    }
-
-    if (wish.isReserved && wish.reservedBy?.id !== viewer.id) {
-        return null;
-    }
-
-    return wish;
-};
-
-export const Wishes = ({ wishes }: WishesProps) => {
-    const viewer = useViewerStore(state => state.user);
-    const store = useStore(viewWishStore);
-
-    if (!wishes) {
-        return (
-            <div className="flex flex-col gap-3">
-                <Subheader>
-                    <Skeleton />
-                </Subheader>
-                <div className="grid grid-cols-4 gap-3">
-                    <Skeleton />
-                    <Skeleton />
-                    <Skeleton />
-                    <Skeleton />
-                </div>
-            </div>
-        );
-    }
-
+export const Wishes = ({ wishes }: { wishes: IWish[] }) => {
     return (
         <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-            {wishes.map(wish => {
-                if (viewer) {
-                    const validatedWish = getVisibleWishOrNull(wish, viewer);
-
-                    if (!validatedWish) {
-                        return null;
-                    }
-
-                    const actions =
-                        wish.owner.id === viewer.id ? (
-                            <PersonalWishActions key={wish.id} />
-                        ) : (
-                            <UserWishActions key={wish.id} />
-                        );
-
-                    const content =
-                        store.type === 'view' ? (
-                            <WishContent actions={actions} wish={wish} />
-                        ) : (
-                            <WishForm
-                                onCancel={() => {
-                                    store.setType('view');
-                                    store.setWish(null);
-                                }}
-                            />
-                        );
-
-                    return (
-                        <WishDialog
-                            key={wish.id + wish.title}
-                            wish={validatedWish}
-                            trigger={<WishCard key={wish.id} wish={wish} />}
-                            content={content}
-                        />
-                    );
-                }
-
-                return (
-                    <WishDialog
-                        key={wish.id + wish.title}
-                        wish={wish}
-                        trigger={<WishCard key={wish.id} wish={wish} />}
-                        content={<WishContent actions={<></>} wish={wish} />}
-                    />
-                );
-            })}
+            {wishes.map(wish => (
+                <Wish key={wish.id} wish={wish} />
+            ))}
         </div>
     );
 };
