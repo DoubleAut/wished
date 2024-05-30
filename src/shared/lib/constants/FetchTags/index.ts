25799e50499e37e3dd4bce765784d8d746ec8904
RichardPickman <drezzerock@gmail.com>
2024-05-30 16:19:50 +0200

    add tags constants for api calls with revalidation

---
+++ src/shared/lib/constants/FetchTags/index.ts
@@ -0,0 +1,7 @@
+export const WISHES_TAG = 'wishes';
+export const RESERVATIONS_TAG = 'reservations';
+export const FRIENDS_TAG = 'friends';
+export const FOLLOWERS_TAG = 'followers';
+export const FOLLOWING_TAG = 'followings';
+export const USERS_TAG = 'users';
+export const USER_TAG = 'user';
