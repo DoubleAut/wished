20bc21f2150182f1839f035e738708af13ee6397
RichardPickman <drezzerock@gmail.com>
2024-06-06 13:59:41 +0200

    split wishes getter to own and users, due to expensive queries in db

--- src/entities/wish/lib/index.ts
+++ src/entities/wish/lib/index.ts
@@ -1,16 +1,32 @@
-import { get } from '@/shared/api/Fetch';
+import { get, remove } from '@/shared/api/Fetch';
+import { WISHES_TAG } from '@/shared/lib/constants/FetchTags';
 import { Wish } from '@/shared/types/Wish';
 
 interface WishesAndReservations {
     wishes: Wish[];
     reservations: Wish[];
+    gifted: Wish[];
+    completed: Wish[];
 }
 
-export const getWishesAndReservations = async (userId: number) => {
-    const response = await get<WishesAndReservations>(`/wishes/${userId}`, [
-        'wishes',
-        'reservations',
-    ]);
+export const getOwnWishes = async (userId: number) => {
+    const response = await get<WishesAndReservations>(
+        `/wishes/own/${userId}`,
+        ['wishes', 'reservations'],
+        true,
+    );
 
     return response;
+};
+
+export const getUserWishes = async (userId: number) => {
+    const response = await get<Wish[]>(`/wishes/${userId}`, ['wishes']);
+
+    return response;
+};
+
+export const deleteImage = async (key: string) => {
+    await remove(`/media/${key}`, [WISHES_TAG], true);
+
+    return { success: true };
 };
