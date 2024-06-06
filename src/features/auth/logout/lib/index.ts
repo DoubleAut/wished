44385f4ccad0b9fe77ee9160fadb7f004350f3ca
RichardPickman <drezzerock@gmail.com>
2024-06-06 15:51:32 +0200

    update logout action

--- src/features/auth/logout/lib/index.ts
+++ src/features/auth/logout/lib/index.ts
@@ -1,10 +1,8 @@
 'use server';
 
-import { removeAccessToken } from '@/shared/api/Fetch/accessToken';
 import { REFRESH_TOKEN_KEY } from '@/shared/lib/constants/localStorage';
 import { cookies } from 'next/headers';
 
 export const logout = () => {
     cookies().delete(REFRESH_TOKEN_KEY);
-    removeAccessToken();
 };
