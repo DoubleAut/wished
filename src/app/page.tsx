ae5984bcb5b0d8281162daef69500a5ca791efde
RichardPickman <drezzerock@gmail.com>
2024-06-06 14:05:15 +0200

    update main page due to wishes tabs integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,54 @@
 'use client';
 
 import { useViewerStore } from '@/core/providers/ViewerProvider';
-import { WishDialog } from '@/entities/wish/ui/WishDialog';
-import { WishForm } from '@/entities/wish/ui/WishForm';
-import { Wishes } from '@/entities/wish/ui/Wishes';
-import { useMemo } from 'react';
+import { Typography } from '@/shared/ui/Text/typography';
+import { DashboardInfoCard } from '@/shared/ui/dashboard-info-card';
+import { WishesTabs } from '@/widgets/wishes/ui/WishesTabs';
+import { RiGiftLine, RiUserLine } from '@remixicon/react';
+import { motion } from 'framer-motion';
 
 const Home = () => {
     const user = useViewerStore(state => state.user);
     const wishes = useViewerStore(state => state.wishes);
     const reservations = useViewerStore(state => state.reservations);
-
-    const allWishes = useMemo(() => {
-        return [...wishes, ...reservations];
-    }, [wishes, reservations]);
+    const gifted = useViewerStore(state => state.gifted);
+    const completed = useViewerStore(state => state.completed);
 
     if (!user) {
         return <div>Loading</div>;
     }
 
     return (
-        <div className="container flex flex-col">
-            <div className="flex items-center space-x-4 uppercase">
-                <p>My wishes</p>
-                <WishDialog
-                    trigger={'Make a wish'}
-                    content={<WishForm onCancel={() => {}} />}
-                    wish={null}
+        <motion.div className="container flex flex-col gap-2">
+            <div className="flex items-center capitalize">
+                <Typography variant="h3">My wishes</Typography>
+            </div>
+            <div className="grid gap-2 md:grid-cols-2 lg:grid-cols-4">
+                <DashboardInfoCard
+                    title="Wishes"
+                    amount={wishes.length}
+                    icon={<RiGiftLine className="h-4 w-4" />}
+                />
+                <DashboardInfoCard
+                    title="Reservations"
+                    amount={reservations.length}
+                    icon={<RiGiftLine className="h-4 w-4" />}
+                />
+                <DashboardInfoCard
+                    title="Gifted"
+                    amount={gifted.length}
+                    icon={<RiGiftLine className="h-4 w-4" />}
+                />
+                <DashboardInfoCard
+                    title="Archived"
+                    amount={completed.length}
+                    icon={<RiUserLine className="h-4 w-4" />}
                 />
             </div>
-            <Wishes wishes={allWishes} />
-        </div>
+            <div className="flex items-center space-x-4 uppercase">
+                <WishesTabs />
+            </div>
+        </motion.div>
     );
 };
 
