ac50f77343698cb37766d4d76a904e1411a77c3e
Richard Pickman <drezzerock@gmail.com>
2024-09-04 15:00:09 +0200

    update Wish type

--- shared/types/Wish/index.ts
+++ shared/types/Wish/index.ts
@@ -1,16 +1,14 @@
 export interface Wish {
-    id: number;
+    id: string;
     title: string;
     description: string;
     price: number;
     canBeAnon: boolean;
     isHidden: boolean;
+    ownerId: string;
     picture: string | null;
     reservedBy: string | null;
-    ownerId: string;
     categoryId: number | null;
     giftDay: string | null;
     isCompleted: boolean;
-    created_at: string;
-    updated_at: string;
 }
