12e32e56b96268d3df2e65c022b0377460f24502
RichardPickman <drezzerock@gmail.com>
2026-03-11 23:52:29 +0100

    add public api for wish entity

---
+++ src/entities/wish/index.ts
@@ -0,0 +1,6 @@
+export { useMyArchived } from './model/useMyArchived';
+export { useMyGifted } from './model/useMyGifted';
+export { useMyReservations } from './model/useMyReservations';
+export { useMyWishes } from './model/useMyWishes';
+
+export { WishForm } from './ui/WishForm';
