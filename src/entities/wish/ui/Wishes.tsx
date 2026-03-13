c142f5c489f54f3c580d904d282db659f0fa949b
RichardPickman <drezzerock@gmail.com>
2026-03-13 13:42:08 +0100

    remove custom filtering on frontend, due to changes of different wish types retrieval. Wishes no longer comeing with props, but getting requested from within. Each list of wishes now has pagination.

--- src/entities/wish/ui/Wishes.tsx
+++ src/entities/wish/ui/Wishes.tsx
@@ -2,133 +2,169 @@
 
 import { useViewerStore } from '@/core/providers/ViewerProvider';
 import { dialogStore } from '@/features/wish/model/dialogView';
-import { CompleteWish } from '@/features/wish/ui/CompleteWish';
-import { DeleteWish } from '@/features/wish/ui/DeleteWish';
-import { EditWish } from '@/features/wish/ui/EditWish';
-import { HideWish } from '@/features/wish/ui/HideWish';
-import { ReserveWish } from '@/features/wish/ui/ReserveWish';
-import { Wish as IWish } from '@/shared/types/Wish';
+import {
+    CompleteWish,
+    DeleteWish,
+    EditWish,
+    HideWish,
+    ReserveWish,
+} from '@/features/wish/ui/Actions';
 import { WishContent } from '@/widgets/wishes/ui/WishContent';
 import { useStore } from 'zustand';
+import { useWishes } from '../model/useWishes';
 import { WishCard } from './WishCard';
 import { WishDialog } from './WishDialog';
 import { WishForm } from './WishForm';
+import { PaginatedWishes } from './WishesPaginated';
 
-export const PersonalWishActions = () => (
-    <div className="flex flex-col justify-end gap-2 sm:flex-row">
-        <CompleteWish />
-        <EditWish />
-        <HideWish />
-        <DeleteWish />
-    </div>
-);
+export const PersonalWishActions = () => {
+    const onAction = () => {};
+
+    return (
+        <div className="flex flex-col justify-end gap-2 sm:flex-row">
+            <CompleteWish onAction={onAction} />
+            <EditWish onAction={onAction} />
+            <HideWish onAction={onAction} />
+            <DeleteWish onAction={onAction} />
+        </div>
+    );
+};
 
 export const UserWishActions = () => (
     <div className="flex flex-col gap-2 sm:flex-row">
-        <ReserveWish />
+        <ReserveWish onAction={() => {}} />
     </div>
 );
 
-export const Wishes = ({ wishes }: { wishes: IWish[] }) => {
+const className = 'grid gap-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4';
+
+// const WishesContainer = ({
+//     pagination,
+//     children,
+// }: {
+//     children: ReactNode;
+//     pagination: WishesPagination;
+// }) => (
+//     <div>
+//         <div className={className}>{children}</div>
+//         <PaginatedWishes pagination={pagination} />
+//     </div>
+// );
+
+export const Wishes = () => {
     const store = useStore(dialogStore);
     const viewer = useViewerStore(state => state.user);
-
-    const visibleWishes = wishes.filter(item => {
-        if (item.owner.id === viewer?.id) {
-            return true;
-        }
-
-        if (!item.reservedBy) {
-            return true;
-        }
-
-        if (item.owner.id === viewer?.id && item.reservedBy) {
-            return true;
-        }
-
-        if (item.reservedBy && item.reservedBy.id === viewer?.id) {
-            return true;
-        }
-
-        return false;
-    });
-
     const isView = store.dialogMode === 'view';
+    const { wishes, pagination, setPage } = useWishes('wishes');
 
-    return visibleWishes.map(wish => (
-        <WishDialog
-            wish={wish}
-            key={wish.id + wish.title}
-            trigger={<WishCard wish={wish} />}
-            content={
-                isView ? (
-                    <WishContent
+    return (
+        <div>
+            <div className={className}>
+                {wishes.map(wish => (
+                    <WishDialog
                         wish={wish}
-                        actions={
-                            viewer?.id === wish.owner.id ? (
-                                <PersonalWishActions key={wish.id} />
+                        key={wish.id + wish.title}
+                        trigger={<WishCard wish={wish} />}
+                        content={
+                            isView ? (
+                                <WishContent
+                                    wish={wish}
+                                    actions={
+                                        viewer?.id === wish.owner.id ? (
+                                            <PersonalWishActions
+                                                key={wish.id}
+                                            />
+                                        ) : (
+                                            <UserWishActions key={wish.id} />
+                                        )
+                                    }
+                                />
                             ) : (
-                                <UserWishActions key={wish.id} />
+                                <WishForm
+                                    onCancel={() => {}}
+                                    onSuccess={() => {}}
+                                />
                             )
                         }
                     />
-                ) : (
-                    <WishForm onCancel={() => {}} />
-                )
-            }
-        />
-    ));
+                ))}
+            </div>
+            <PaginatedWishes pagination={pagination} onPageChange={setPage} />
+        </div>
+    );
 };
 
-export const ReservedWishes = ({ wishes }: { wishes: IWish[] }) => {
-    const items = wishes.filter(item => !item.isCompleted);
+export const ReservedWishes = () => {
+    const { wishes, pagination, setPage } = useWishes('reservations');
 
-    return items.map(wish => (
-        <WishDialog
-            wish={wish}
-            key={wish.id + wish.title}
-            trigger={<WishCard wish={wish} />}
-            content={
-                <WishContent
-                    wish={wish}
-                    actions={<UserWishActions key={wish.id} />}
-                />
-            }
-        />
-    ));
+    return (
+        <div>
+            <div className={className}>
+                {wishes.map(wish => (
+                    <WishDialog
+                        wish={wish}
+                        key={wish.id + wish.title}
+                        trigger={<WishCard wish={wish} />}
+                        content={
+                            <WishContent
+                                wish={wish}
+                                actions={<UserWishActions key={wish.id} />}
+                            />
+                        }
+                    />
+                ))}
+            </div>
+            <PaginatedWishes pagination={pagination} onPageChange={setPage} />
+        </div>
+    );
 };
 
-export const GiftedWishes = ({ wishes }: { wishes: IWish[] }) => {
-    const items = wishes.filter(item => item.isCompleted);
+export const GiftedWishes = () => {
+    const { wishes, pagination, setPage } = useWishes('gifted');
 
-    return items.map(wish => (
-        <WishDialog
-            wish={wish}
-            key={wish.id + wish.title}
-            trigger={<WishCard wish={wish} />}
-            content={<WishContent wish={wish} actions={<></>} />}
-        />
-    ));
+    return (
+        <div>
+            <div className={className}>
+                {wishes.map(wish => (
+                    <WishDialog
+                        wish={wish}
+                        key={wish.id + wish.title}
+                        trigger={<WishCard wish={wish} />}
+                        content={
+                            <WishContent
+                                wish={wish}
+                                actions={<UserWishActions key={wish.id} />}
+                            />
+                        }
+                    />
+                ))}
+            </div>
+            <PaginatedWishes pagination={pagination} onPageChange={setPage} />
+        </div>
+    );
 };
 
-export const ArchivedWishes = ({ wishes }: { wishes: IWish[] }) => {
-    const items = wishes.filter(item => item.isCompleted);
+export const ArchivedWishes = () => {
+    const { wishes, pagination, setPage } = useWishes('archived');
 
-    return items.map(wish => (
-        <WishDialog
-            wish={wish}
-            key={wish.id + wish.title}
-            trigger={<WishCard wish={wish} />}
-            content={
-                <WishContent
-                    wish={wish}
-                    actions={
-                        <div className="flex flex-col gap-2 sm:flex-row">
-                            <DeleteWish />
-                        </div>
-                    }
-                />
-            }
-        />
-    ));
+    return (
+        <div>
+            <div className={className}>
+                {wishes.map(wish => (
+                    <WishDialog
+                        wish={wish}
+                        key={wish.id + wish.title}
+                        trigger={<WishCard wish={wish} />}
+                        content={
+                            <WishContent
+                                wish={wish}
+                                actions={<UserWishActions key={wish.id} />}
+                            />
+                        }
+                    />
+                ))}
+            </div>
+            <PaginatedWishes pagination={pagination} onPageChange={setPage} />
+        </div>
+    );
 };
